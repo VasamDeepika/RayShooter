6220799d9d42aabac848ef38475f871ee2bdb260
DeepikaVasam123 <deepika.vasam@xcubelabs.com>
2021-06-10 07:50:44 -0700

    Player Movement and rotations added

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -5,19 +5,59 @@ using UnityStandardAssets.CrossPlatformInput;
 
 public class Movement : MonoBehaviour
 {
+    [SerializeField]
+    float speed;
+    float xRange = 16.0f;
+    float yRange = 5.0f;
+    [SerializeField]
+    float positionpitchFactor = 5.0f;
+    [SerializeField]
+    float controlpitchFactor = 5f;
+    float pitchControlValue = 5f;
+    [SerializeField]
+    float positionyawFactor = 5f;
+    [SerializeField]
+    float controlrollFactor = 5f;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
-
+    float xOffset, yOffset;
     // Update is called once per frame
     void Update()
     {
-
-        float horizontalMove = CrossPlatformInputManager.GetAxis("Horizontal");
-        Debug.Log(horizontalMove);
+        playerPosition();
+        playerRotation();
     }
+     private void playerRotation()
+    {
+        //float xRotation = transform.localPosition.x * rotationFactor;
+        float yRotation = transform.localPosition.y * positionpitchFactor;
+        float pitchControlValue = yOffset * controlpitchFactor;
+        float pitch = yRotation + pitchControlValue;
+        
 
+        float yaw = transform.localPosition.x * positionyawFactor;
+
+        float roll = xOffset * controlrollFactor;
+
+        transform.localRotation = Quaternion.Euler(pitch, yaw, roll);
+    }
+    private void playerPosition()
+    {
+        float horizontalMove = CrossPlatformInputManager.GetAxis("Horizontal");
+
+        float xOffset = horizontalMove * speed * Time.deltaTime;
+
+        float verticalMove = CrossPlatformInputManager.GetAxis("Vertical");
+
+        float yOffset = verticalMove * speed * Time.deltaTime;
+
+        float XrawPos = transform.localPosition.x + xOffset;
+
+        float YrawPos = transform.localPosition.y + yOffset;
+
+        float clampedXpos = Mathf.Clamp(XrawPos, -xRange, xRange);
+
+        float clampedYpos = Mathf.Clamp(YrawPos, -yRange, yRange);
+
+        transform.localPosition = new Vector3(clampedXpos, clampedYpos, transform.localPosition.z);
+    }
 }
